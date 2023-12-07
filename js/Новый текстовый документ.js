9bad161d53d600072040b4bd8529b29bdec6dfe5
Школьник <school@college.spk>
2023-12-07 13:27:35 +0700

    firstcommit

---
+++ js/Новый текстовый документ.js
@@ -0,0 +1,96 @@
+/*alert("Напишите свой возвраст")
+
+let age = prompt("Введите ваш возвраст")
+
+function AGE (age){
+    if (age >= 0 && age <= 3)
+    {
+        alert("lox ti zabanen!")
+    }
+    else if (age >= 4 && age <= 5)
+    {
+        alert("Иди в садик!")
+    }
+    else if (age >= 6 && age <= 15)
+    {
+        alert("го ту скул!")
+    }
+    else if (age >= 16 && age <= 24 ) 
+    {
+        alert("Go to sharaga ili go tu ynever")
+    }   
+    else if (age >= 25 && age <=54)
+    {
+        alert("Go to work")
+    }
+    else if (age >= 55 && age <= 64)
+    {
+        alert("go to bolnica")
+    }
+    else if (age >= 65 && age <81)
+    {
+        alert("Готовь могилу")
+    }
+    else if (age >= -9999999 || age <= -1 || age >= 99999999 || age <= 100)
+    {
+        alert("che dyraK?")
+    }
+}
+AGE (age)*/
+
+/*var name = prompt("Name")
+
+function NAME (name)
+{
+    if (name == "Антон" || name == "Денис")
+    {
+        console.log("ВЫЙДИ ОТСЮДА!")
+    }
+    else if ( name )
+    {
+        console.log("САЛАМ АЛЕЙКУМ НИЗАР")
+    }
+}
+NAME(name)*/
+/*var ZAVA = 0;
+
+function JOPA ()
+{
+    ZAVA ++;
+    if(ZAVA >=5)
+{
+    alert("Вы вызвали метод больше 5 раз")
+}
+    else
+{
+        alert(ZAVA)
+}
+}
+JOPA()
+JOPA()
+JOPA()
+JOPA()
+JOPA()
+JOPA()*/
+
+/*alert("Введите градусы воды")
+
+var gradys = prompt("Введите градус")
+
+function GRADYS (gradys)
+{
+    if ( gradys <= 0)
+    {
+        alert("Вода превратилась в лед")
+    }
+    else if (gradys >= 1 && gradys <=100)
+    {
+        alert("Это простая водичка")
+    }
+    else if (gradys > 101)
+    {
+        alert("Вода превращается в пар")
+    }
+}
+GRADYS(gradys)*/
+
